ad55203f7caee0a87e3bb5476dac7b371b95bfbb
rafael <rafaeljosegomes25@gmail.com>
2022-04-26 19:06:11 -0300

    created choice for post new tag

--- script.sh
+++ script.sh
@@ -3,14 +3,21 @@ token=`grep TOKEN .env | cut -d"=" -f2`
 owner=`grep OWNER .env | cut -d"=" -f2`
 repo=`grep REPO .env | cut -d"=" -f2`
 
-echo "Informe o título da tag: "
-read title
-echo "Informe a mensagem da tag: "
-read message
+echo "Deseja gerar uma nova tag? (s/n)"
 
-curl \
-  -u $username:$token \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$owner/$repo/git/tags \
-  -d '{"tag":"'$title'","message":"'$message'", "object":"83317ad8dc404a11af09689cc365225e88bdb765", "type":"commit"}'
+read choice
+
+if [ "$choice" = "s" ]
+then
+    echo "Informe o título da tag: "
+    read title
+    echo "Informe a mensagem da tag: "
+    read message
+
+    curl \
+    -u $username:$token \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/$owner/$repo/git/tags \
+    -d '{"tag":"'$title'","message":"'$message'", "object":"83317ad8dc404a11af09689cc365225e88bdb765", "type":"commit"}'
+fi
