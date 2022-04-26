279866e8ddfa35c92b7cd9143d72b2bbcee2a108
rafael <rafaeljosegomes25@gmail.com>
2022-04-26 18:55:56 -0300

    initial commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+username=`grep USERNAME .env | cut -d"=" -f2`
+token=`grep TOKEN .env | cut -d"=" -f2`
+owner=`grep OWNER .env | cut -d"=" -f2`
+repo=`grep REPO .env | cut -d"=" -f2`
+
+echo "Informe o título da tag: "
+read title
+echo "Informe a mensagem da tag: "
+read message
+
+curl \
+  -u $username:$token \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/$owner/$repo/git/tags \
+  -d '{"tag":"'$title'","message":"'$message'", "object":"83317ad8dc404a11af09689cc365225e88bdb765", "type":"commit"}'
